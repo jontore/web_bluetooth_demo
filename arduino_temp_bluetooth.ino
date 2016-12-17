7448e288ed7503d174fa2853d2bd12b61ca1d31b
Jon Tore Hafstad <jontore@gmail.com>
2016-12-17 19:33:18 +0100

    Arduino displays temp on lcd display

--- arduino_temp_bluetooth.ino
+++ arduino_temp_bluetooth.ino
@@ -1,5 +1,7 @@
 
+#include <Wire.h>
 #include <CurieBLE.h>
+#include "rgb_lcd.h"
 
 BLEPeripheral blePeripheral;       // BLE Peripheral Device (the board you're programming)
 BLEService tempService("72664d13-e5bd-4dfd-b184-6fa5b5f9c2e6");
@@ -14,35 +16,45 @@ long previousMillis = 0;
 const int B=4275;                 // B value of the thermistor
 const int R0 = 100000;            // R0 = 100k
 
+rgb_lcd lcd;
+
+const int colorR = 255;
+const int colorG = 255;
+const int colorB = 255;
 
 void setup() {
   Serial.begin(9600);
   pinMode(4, OUTPUT);
 
-  Serial.println("INit.");
+  Serial.println("Init.");
+  //Setup bluetooth
   blePeripheral.setLocalName("TempMonitorSketch");
   blePeripheral.setAdvertisedServiceUuid(tempService.uuid());
   blePeripheral.addAttribute(tempService);
   blePeripheral.addAttribute(tempCharacteristic);
   tempCharacteristic.setValue(oldTemperature);
 
-  /* Now activate the BLE device.  It will start continuously transmitting BLE
-     advertising packets and will be visible to remote BLE central devices
-     until it receives a new connection */
+  //Setup display
+  lcd.begin(16, 2);
+  lcd.setRGB(colorR, colorG, colorB);
+
   blePeripheral.begin();
-  Serial.println("Bluetooth device active, waiting for connections...");
+  lcd.print("Bluetooth device active, waiting for connections...");
 }
 
 void loop() {
   // listen for BLE peripherals to connect:
   BLECentral central = blePeripheral.central();
 
-  Serial.println("Wait");
+  //Print temp while waiting
+  float newTemperature = readTemperature();
+  printTemperature(newTemperature);
+
   // if a central is connected to peripheral:
   if (central) {
-    Serial.print("Connected to central: ");
+    lcd.print("Connected to central: ");
     // print the central's MAC address:
-    Serial.println(central.address());
+    lcd.println(central.address());
     // turn on the LED to indicate the connection:
     digitalWrite(4, HIGH);
 
@@ -53,7 +65,9 @@ void loop() {
       // if 200ms have passed, check the battery level:
       if (currentMillis - previousMillis >= 200) {
         previousMillis = currentMillis;
-        updateTempLevel();
+        float newTemperature = readTemperature();
+        printTemperature(newTemperature);
+        updateTempLevel(newTemperature);
       }
     }
     // when the central disconnects, turn off the LED:
@@ -61,21 +75,30 @@ void loop() {
     Serial.print("Disconnected from central: ");
     Serial.println(central.address());
   }
+
+   delay(200);
 }
 
-void updateTempLevel() {
+float readTemperature() {
   int val = analogRead(A0);
 
-  float R = 1023.0/((float)val)-1.0;
+  float R = 1023.0/((float)val*0.66)-1.0;
   R = 100000.0*R;
 
-  float temperature = 1.0/(log(R/100000.0)/B+1/298.15)-273.15;//convert to temperature via datasheet ;
+  return (1.0/(log(R/100000.0)/B+1/298.15)-273.15);
+}
 
-
+void updateTempLevel(float temperature) {
   if (temperature != oldTemperature) {
     Serial.print("Temp is % is now: ");
     Serial.println(temperature);
     tempCharacteristic.setValue(temperature);
     oldTemperature = temperature;
   }
+}
+
+void printTemperature(float temperature) {
+  lcd.setCursor(0, 0);
+  lcd.clear();
+  lcd.print(temperature);
 }
