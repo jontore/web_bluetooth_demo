360da620ab9f75de253336a603ba470ae12b6ffe
Jon Tore Hafstad <jontore@gmail.com>
2017-06-03 13:26:07 +0200

    Decoding value correctly from characteristic

--- reactNativeThermo/derp.js
+++
@@ -1,8 +0,0 @@
-var Buffer = require('buffer').Buffer;
-var base64 = require('base-64');
-
-var value = "623IQQ=="
-var valueAscii = new Buffer(value, "base64").toString('utf8')
-var v = new Buffer(value, "base64").toString('hex')
-
-console.log("ascii", valueAscii, "hex", v, parseFloat(v, 16), base64.decode(value));
